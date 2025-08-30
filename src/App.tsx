3c4c7f6f7473842de4e6a13ff1f2b1b7a0b08547
Brett <bocksteink@gmail.com>
2025-08-30 12:55:23 -0400

    Build/Refactor: Changed underlying stack to use vite instead of next.js

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import Navigation from './components/Navigation'
+import HeroSection from './components/HeroSection'
+import ProjectsSection from './components/ProjectsSection'
+import ExperienceSection from './components/ExperienceSection'
+import SkillsSection from './components/SkillsSection'
+import HomelabSection from './components/HomelabSection'
+import BlogSection from './components/BlogSection'
+import ContactSection from './components/ContactSection'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-dark-900 text-white">
+      <Navigation />
+      <main>
+        <HeroSection />
+        <ProjectsSection />
+        <ExperienceSection />
+        <SkillsSection />
+        <HomelabSection />
+        <BlogSection />
+        <ContactSection />
+      </main>
+    </div>
+  )
+}
+
+export default App
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
